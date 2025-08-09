a92cd6a86a952eafcd3ba5d9ab4bd877b761037f
Gabriel Soares <gabrielsoacc@gmail.com>
2025-08-09 19:48:37 -0300

    feat: created header stock page

---
+++ src/app/components/stock/stock.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InfoCard } from "../../shared/info-card/info-card";
+
+@Component({
+  selector: 'app-stock',
+  imports: [InfoCard],
+  templateUrl: './stock.html',
+  styleUrl: './stock.scss'
+})
+export class Stock {
+
+}
