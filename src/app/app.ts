7d994b7a2eaf350adf426cb9b134c3839c0fe042
Gabriel Soares <gabrielsoacc@gmail.com>
2025-08-09 22:07:41 -0300

    feat: created initial stock page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoCard } from "./shared/info-card/info-card";
+import { Stock } from "./components/stock/stock";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InfoCard],
+  imports: [RouterOutlet, Stock],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
